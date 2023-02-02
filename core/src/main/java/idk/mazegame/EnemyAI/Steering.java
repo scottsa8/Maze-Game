59291c677288bf562133e875d55906c7f6d67f18
scottsa8 <s.a.smith5@lancaster.ac.uk>
2023-02-02 22:31:19 +0000

    added file and basic methods for enemy AI

---
+++ core/src/main/java/idk/mazegame/EnemyAI/Steering.java
@@ -0,0 +1,136 @@
+package idk.mazegame.EnemyAI;
+
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+public class Steering implements Steerable<Vector2>
+{
+
+    @Override
+    public Vector2 getPosition() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getOrientation() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setOrientation(float orientation) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Location<Vector2> newLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getZeroLinearSpeedThreshold() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setZeroLinearSpeedThreshold(float value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float getMaxLinearSpeed() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setMaxLinearSpeed(float maxLinearSpeed) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float getMaxLinearAcceleration() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setMaxLinearAcceleration(float maxLinearAcceleration) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float getMaxAngularSpeed() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setMaxAngularSpeed(float maxAngularSpeed) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float getMaxAngularAcceleration() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setMaxAngularAcceleration(float maxAngularAcceleration) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Vector2 getLinearVelocity() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getAngularVelocity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public float getBoundingRadius() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isTagged() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setTagged(boolean tagged) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
