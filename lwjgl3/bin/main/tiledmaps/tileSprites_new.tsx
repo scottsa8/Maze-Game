870520b43939e3db7f61e36ee9d43b853242f9e9
Zim <98758788+hazimbharis@users.noreply.github.com>
2023-02-27 17:18:57 +0000

    deleted lwjgl3 bin

--- lwjgl3/bin/main/tiledmaps/tileSprites_new.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tileSprites_new" tilewidth="16" tileheight="16" tilecount="154" columns="11">
- <image source="tileSprites_new.png" width="176" height="224"/>
-</tileset>
