b804c864924544bd4c86b0fc7b593f08721a45c2
Zim <98758788+hazimbharis@users.noreply.github.com>
2023-03-14 22:17:19 +0000

    changed node passing

--- core/src/main/java/idk/mazegame/EnemyAI/PathFinding/graphPath.java
+++ core/src/main/java/idk/mazegame/EnemyAI/PathFinding/graphPath.java
@@ -6,26 +6,12 @@ import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
 import com.badlogic.gdx.math.Vector2;
 
 public class graphPath{
-    public static GraphPath<Node> getPath(Vector2 entityPos, Vector2 targetPos)
+    public static GraphPath<Node> getPath(Node entityPos, Node targetPos)
     {
-        Node start = null;
-        Node end = null;
+        Node start = entityPos;
+        Node end = targetPos;
         Graph graph = PathFindingSystem.graph;
         DistanceHeuristic dh = PathFindingSystem.dh;
-        for(Node node:graph.getNodes())
-        {
-            if(Math.abs(node.pos.x - entityPos.x) < 8
-            && Math.abs(node.pos.y - entityPos.y) < 8)
-            {
-                start = node;
-            }
-            if(Math.abs(node.pos.x - targetPos.x) < 8
-            && Math.abs(node.pos.y - targetPos.y) < 8)
-            {
-                end = node;
-            }
-        }
-      // System.out.println(entityPos + " , " + start.pos + " || " + targetPos + " , " + end.pos);
     
         GraphPath<Node> path =  new DefaultGraphPath<Node>();
         new IndexedAStarPathFinder<>(graph).searchNodePath(start, end, dh, path);
