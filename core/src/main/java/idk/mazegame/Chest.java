a4e6921e67fabcd43995b0ddc941d94bebf50c02
scott <s.a.smith5@lancaster.ac.uk>
2023-03-14 14:08:40 +0000

    chest texture now spawns with chest, and adjusted hitbox for it

--- core/src/main/java/idk/mazegame/Chest.java
+++ core/src/main/java/idk/mazegame/Chest.java
@@ -12,8 +12,8 @@ public class Chest {
     private Body body;
 
     public Chest(World world, float x, float y) {
-        textureAtlas = new TextureAtlas("enemy/zombieSprites.atlas");
-        chestSprite = new Sprite(textureAtlas.findRegion("zombieDown",1));
+        textureAtlas = new TextureAtlas("items/chest.atlas");
+        chestSprite = new Sprite(textureAtlas.findRegion("chest",-1));
         chestSprite.setPosition(Gdx.graphics.getWidth()/2 - chestSprite.getWidth()/2, Gdx.graphics.getHeight()/2 - chestSprite.getHeight()/2);
         this.body = createBody(world,x,y);
     }
@@ -22,11 +22,11 @@ public class Chest {
     {
         Body b;
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.type = BodyDef.BodyType.StaticBody;
         bodyDef.position.set((x + chestSprite.getWidth()/2), (y- chestSprite.getHeight()/2));
         b = world.createBody(bodyDef);
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(chestSprite.getWidth()/2 / Constants.PPM +1, chestSprite.getHeight()/2 / Constants.PPM);
+        shape.setAsBox(chestSprite.getWidth()/2 / Constants.PPM +4, chestSprite.getHeight()/2 / Constants.PPM+2);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
         fixtureDef.density = 1f;
