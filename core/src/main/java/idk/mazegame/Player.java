9634d0b6659f32c44ac9e8d03b753b5868c7f04c
Zim <98758788+hazimbharis@users.noreply.github.com>
2023-03-17 00:35:46 +0000

    fixed attack positioning for melee

--- core/src/main/java/idk/mazegame/Player.java
+++ core/src/main/java/idk/mazegame/Player.java
@@ -832,7 +832,7 @@ public class Player {
             pos = new Vector2(getPlayerSprite().getWidth()/2 / Constants.PPM - 2.5f, getPlayerSprite().getHeight()/2 / Constants.PPM -3);
         }
           
-        attackCircle = ShapeMaker.createSquare(new Vector2(getPlayerSprite().getX() + 7.5f, getPlayerSprite().getY() + 4f),pos, true, world);
+        attackCircle = ShapeMaker.createSquare(new Vector2(getPlayerSprite().getX() + pos.x*2, getPlayerSprite().getY() + pos.y*2),new Vector2(0,0), true, world);
         attackCircle.setUserData(name.toString());
 
         Timer timer=new Timer();
@@ -916,8 +916,9 @@ public class Player {
             dir = new Vector2(2,0); 
         }
 
-            dest = ShapeMaker.createSquare(new Vector2(getPlayerSprite().getX() + 7.5f, getPlayerSprite().getY() + 4f),
-            pos, true, world);
+            dest = ShapeMaker.createSquare(new Vector2(getPlayerSprite().getX() + pos.x*2, getPlayerSprite().getY() + pos.y*2),
+                    new Vector2(0,0),
+                    true, world);
             dest.setUserData("dest");
             
             MazeGame.entities.add(new Projectile(world,new Vector2(getPlayerSprite().getX() + 7.5f, getPlayerSprite().getY() + 4f),dir,ammo,
